668876980d3887cbf8bb0fc07b6996c569e656a2
kost9klinov <kost9klinov>
2018-06-27 18:05:25 +0300

    added outlets

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tipPercentageLbl: UILabel!
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    @IBOutlet weak var tipLbl: UILabel!
+    @IBOutlet weak var totalLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
